2a8dd1585f82e1219f78af9e5f299e3943d07fb0
Yoshimi Kosugi <keepyourself75@gmail.com>
2017-08-08 17:03:26 +0000

    20170809_DIVE_13

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -5,6 +5,7 @@ class PoemsController < ApplicationController
   # GET /poems.json
   def index
     @poems = Poem.all
+  #  raise
   end
 
   # GET /poems/1
@@ -14,7 +15,7 @@ class PoemsController < ApplicationController
 
   # GET /poems/new
   def new
-    @poem = Poem.new
+   @poem = Poem.new
   end
 
   # GET /poems/1/edit
@@ -41,6 +42,7 @@ class PoemsController < ApplicationController
   # PATCH/PUT /poems/1.json
   def update
     respond_to do |format|
+      @poem = Poem.find(params[:id])
       if @poem.update(poem_params)
         format.html { redirect_to @poem, notice: 'Poem was successfully updated.' }
         format.json { render :show, status: :ok, location: @poem }
@@ -69,6 +71,6 @@ class PoemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poem_params
-      params.require(:poem).permit(:content, :auther)
+      params.require(:poem).permit(:title, :content, :auther)
     end
 end
